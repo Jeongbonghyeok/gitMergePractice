74a48876bc7f2f9a0f88100d573d4ec18f4c9a96
Bong <bongithub02@gmail.com>
2025-11-18 21:29:39 +0900

    merge modify

---
+++ sub2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;    
+
+void greet(){
+    cout << "Hello from sub.cpp!" << endl;
+}
+<<<<<<< HEAD
+void fuckYou(){
+    cout <<"Fuck You!" << endl;
+}
+=======
+>>>>>>> A
+
+int main(){
+    greet();
+    fuckYou();
+    return 0;
+}
